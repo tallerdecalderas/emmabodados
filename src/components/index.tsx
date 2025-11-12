bc47a0ecc8ccb0d57e4effc6c3a27de674792521
fxcx <facundo19burgos@gmail.com>
2025-11-12 01:43:09 -0300

    setup / @configuracion de rutas

---
+++ src/components/index.tsx
@@ -0,0 +1,2 @@
+export { Hero } from './home/hero/hero';
+export { default as Nav } from './home/nav/nav'; 
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "@/components/home/hero/hero"
+
+export default function Home () {
+    return (
+        <div>
+            Home Page
+            <div>
+                <Hero/>
+            </div>
+        </div>
+    )
+}
