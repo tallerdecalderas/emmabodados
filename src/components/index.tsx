344aa292676b2770408d528d63d734fce5e80a34
fxcx <facundo19burgos@gmail.com>
2025-11-13 01:08:19 -0300

    archivos barreal de importacion

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,5 @@
+export { Header } from './header/header';
 export { Hero } from './home/hero/hero';
-export { default as Nav } from './home/nav/nav'; 
+export { default as Nav } from './home/nav/nav';
+export { Footer} from './footer/footer'
+export {Logo} from './home/logo/logo'
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
+// aca pruebo como de se hace el import si esta con default ** Import barreal
 export { default as Home } from './home/home'
