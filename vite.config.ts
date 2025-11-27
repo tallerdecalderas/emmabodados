86f0f1fef28e7260e5aaccffe1292a6dcfe0db1b
taller <tallerdecalderasoficial@gmail.com>
2025-11-27 19:19:07 +0000

    fix: update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: "/emmabodados/",
+  base: "/https://github.com/tallerdecalderas/emmabodados",
   plugins: [react(),
   tailwindcss(),
   ],
